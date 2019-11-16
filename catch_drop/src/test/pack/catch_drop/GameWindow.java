c81c5047dde3dd37d630aed0764f65a1ccf8046a
Alexander Krasnov <rn4acy@mail.ru>
2019-11-16 22:17:29 +0300

    Добавлено событие по клику мыши
    Клик по капле генерит новую каплю, увлеичивая скорость ее перемещения

--- catch_drop/src/test/pack/catch_drop/GameWindow.java
+++ catch_drop/src/test/pack/catch_drop/GameWindow.java
@@ -3,6 +3,8 @@ package test.pack.catch_drop;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class GameWindow extends JFrame {
@@ -12,7 +14,7 @@ public class GameWindow extends JFrame {
     private static Image drop;
     private static Image gameOver;
     private static float drop_left = 200; // координата Х капли
-    private static float drop_top = -100; // координата У капли
+    private static float drop_top = -100; // координата У капли (верхняя граница капли)
     private static float drop_v = 200; // Скорость капли
 
     public static void main(String[] args) throws IOException {
@@ -29,6 +31,21 @@ public class GameWindow extends JFrame {
         game_window.setResizable(false);
         last_frame_time = System.nanoTime(); // присваиваем началное значение в наносекундах (текущее время)
         GameField game_field = new GameField();
+        game_field.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) { // mousePressed вызывается тогда. когда произойдет нажатие мыши
+                int x = e.getX(); // получаем координаты нажатия мыши
+                int y = e.getY();
+                float drop_right = drop_left + drop.getWidth(null); // узнаем ширину капли
+                float drop_bottom = drop_top + drop.getHeight(null); // узнаем высоту капли, чтобы понять. попала ли мышь туда
+                boolean is_drop = x >= drop_left && x <= drop_right && y >= drop_top && y <= drop_bottom; // если это выражение true, то is_drop = тру, если false, то is_drop = фолс
+                if (is_drop) {
+                    drop_top = -100; // Если попали, то ресетим каплю за пределы окна
+                    drop_left = (int) (Math.random() * (game_field.getWidth() - drop.getWidth(null))); // Генерируем рандомное положение по горизонтали в пределах ширины поля
+                    drop_v = drop_v + 20; // увеличиваем скорость движения капли
+                }
+            }
+        });
         game_window.add(game_field);
         game_window.setVisible(true);
     }
@@ -36,14 +53,14 @@ public class GameWindow extends JFrame {
         long current_time = System.nanoTime(); // Определяем текущее время
         float delta_time = (current_time - last_frame_time) * 0.000000001f; // вычисляем дельту и переводим наносек в сек
         last_frame_time = current_time; // время предыдущего кадра = текущее время
-        drop_top = drop_top + drop_v * delta_time; // вычисляем текущую координату и присваиваем к капле
-        drop_left = drop_left + drop_v * delta_time;
+        drop_top = drop_top + drop_v * delta_time; // вычисляем текущую координату и присваиваем к капле для полета сверху вниз
+        //drop_left = drop_left + drop_v * delta_time;
 
         //g.fillOval(10,10, 200, 100); // рисуем овал
         //g.drawLine(20,20,70,70); // рисуем линию
         g.drawImage(background, 0, 0, null); // рисуем фон, ЛВ угол в точке 0 0
         g.drawImage(drop, (int) drop_left, (int) drop_top, null); // рисуем каплю. конвертим float в int, чтобы работало
-        //g.drawImage(gameOver, 280, 120, null);
+        if (drop_top > game_window.getHeight()) g.drawImage(gameOver, 280, 120, null); // когда капля вылетает за границу окна, показать гейм овер
     }
 
     private static class GameField extends JPanel {
