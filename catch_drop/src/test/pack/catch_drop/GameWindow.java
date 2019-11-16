e74dadc290c97078212f3efaeff3ef21f9f277cd
Alexander Krasnov <rn4acy@mail.ru>
2019-11-16 21:10:16 +0300

    Добавлены картинки, фон. короче контент
    картинки статичные

--- catch_drop/src/test/pack/catch_drop/GameWindow.java
+++ catch_drop/src/test/pack/catch_drop/GameWindow.java
@@ -1,11 +1,23 @@
 package test.pack.catch_drop;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame {
     private static GameWindow game_window; // GameWindow here is a type of the variable game_window
-    public static void main(String[] args) {
+    private static Image background;
+    private static Image drop;
+    private static Image gameOver;
+
+    public static void main(String[] args) throws IOException {
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png")); // Загружаем картинки используя метод read io
+        // а так как read может вызвать исключение, он подчеркнут. Поэтому добавляем в метод main сигнатуру throws IOException
+        // загружаем оставшиеся картинки
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
+        gameOver = ImageIO.read(GameWindow.class.getResourceAsStream("gameover.png"));
+
         game_window = new GameWindow(); // Create object from GameWindow class and put it to variable
         game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //When closing window, program will closed
         game_window.setLocation(200, 100);
@@ -16,8 +28,12 @@ public class GameWindow extends JFrame {
         game_window.setVisible(true);
     }
     private static void onRepaint(Graphics g) {
-        g.fillOval(10,10, 200, 100);
-        g.drawLine(20,20,70,70);
+        //g.fillOval(10,10, 200, 100); // рисуем овал
+        //g.drawLine(20,20,70,70); // рисуем линию
+        g.drawImage(background, 0, 0, null); // рисуем фон, ЛВ угол в точке 0 0
+        g.drawImage(drop, 100, 100, null);
+        g.drawImage(gameOver, 280, 120, null);
+
     }
     private static class GameField extends JPanel {
 
